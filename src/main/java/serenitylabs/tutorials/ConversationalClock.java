d9a82f36f0b987f9c14e833ce71843afaa4d21ee
rooneyc <conor.p.rooney@aib.ie>
2017-04-23 11:41:49 +0100

    midnight and noon

--- src/main/java/serenitylabs/tutorials/ConversationalClock.java
+++ src/main/java/serenitylabs/tutorials/ConversationalClock.java
@@ -4,6 +4,8 @@ public class ConversationalClock {
 
     private final SystemTime now;
 
+    final String onAnHourSuffix = "o'clock";
+
     public ConversationalClock(SystemTime time) {
         this.now = time;
     }
@@ -15,14 +17,56 @@ public class ConversationalClock {
         String hourString = "";
 
         switch (hour) {
-            case 1: hourString = "one";
+            case 1: hourString = "one " + onAnHourSuffix;
                     break;
-            case 2: hourString = "two";
+            case 2: hourString = "two " + onAnHourSuffix;
                 break;
-            case 13: hourString = "one";
+            case 3: hourString = "three " + onAnHourSuffix;
+                break;
+            case 4: hourString = "four " + onAnHourSuffix;
+                break;
+            case 5: hourString = "five " + onAnHourSuffix;
+                break;
+            case 6: hourString = "six " + onAnHourSuffix;
+                break;
+            case 7: hourString = "seven " + onAnHourSuffix;
+                break;
+            case 8: hourString = "eight " + onAnHourSuffix;
+                break;
+            case 9: hourString = "nine " + onAnHourSuffix;
+                break;
+            case 10: hourString = "ten " + onAnHourSuffix;
+                break;
+            case 11: hourString = "eleven " + onAnHourSuffix;
+                break;
+            case 12: hourString = "noon";
+                break;
+            case 13: hourString = "one " + onAnHourSuffix;
+                break;
+            case 14: hourString = "two " + onAnHourSuffix;
+                break;
+            case 15: hourString = "three " + onAnHourSuffix;
+                break;
+            case 16: hourString = "four " + onAnHourSuffix;
+                break;
+            case 17: hourString = "five " + onAnHourSuffix;
+                break;
+            case 18: hourString = "six " + onAnHourSuffix;
+                break;
+            case 19: hourString = "seven " + onAnHourSuffix;
+                break;
+            case 20: hourString = "eight " + onAnHourSuffix;
+                break;
+            case 21: hourString = "nine " + onAnHourSuffix;
+                break;
+            case 22: hourString = "ten " + onAnHourSuffix;
+                break;
+            case 23: hourString = "eleven " + onAnHourSuffix;
+                break;
+            case 24: hourString = "midnight";
                 break;
         }
 
-        return "it's " + hourString + " o'clock";
-    }
+        return "it's " + hourString;
+}
 }
--- src/test/java/serenitylabs/tutorials/ConversationalClockTest.java
+++ src/test/java/serenitylabs/tutorials/ConversationalClockTest.java
@@ -14,8 +14,29 @@ public class ConversationalClockTest {
 
     @TestWith({
             "01:00, it's one o'clock",
-            "13:00, it's one o'clock",
             "02:00, it's two o'clock",
+            "03:00, it's three o'clock",
+            "04:00, it's four o'clock",
+            "05:00, it's five o'clock",
+            "06:00, it's six o'clock",
+            "07:00, it's seven o'clock",
+            "08:00, it's eight o'clock",
+            "09:00, it's nine o'clock",
+            "10:00, it's ten o'clock",
+            "11:00, it's eleven o'clock",
+            "12:00, it's noon",
+            "13:00, it's one o'clock",
+            "14:00, it's two o'clock",
+            "15:00, it's three o'clock",
+            "16:00, it's four o'clock",
+            "17:00, it's five o'clock",
+            "18:00, it's six o'clock",
+            "19:00, it's seven o'clock",
+            "20:00, it's eight o'clock",
+            "21:00, it's nine o'clock",
+            "22:00, it's ten o'clock",
+            "23:00, it's eleven o'clock",
+            "24:00, it's midnight",
     })
     public void should_tell_the_time_on_the_hour(ConversationalClock clock, String expectedTime) throws Exception {
         assertThat(clock.currentTime()).isEqualTo(expectedTime);
